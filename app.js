63c701913028d5d8b3be57dbb15503c6a9de9c01
rohit-navsoft <rohit.bhagat@navsoft.in>
2021-06-26 16:59:02 +0530

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const createError = require("http-errors");
+const cors = require('cors');
 require("dotenv").config();
 require('./helpers/db-connection')();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const errorHandler = require('./error/error-handler');
 const userRoute = require('./users/user.route');
 
 app.use(express.json())
+app.use(cors());
 app.use(morgan('combined'));
 // routes
 app.use('/auth', userRoute);
